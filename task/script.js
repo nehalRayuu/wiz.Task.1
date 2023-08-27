e17a063334c2dddbddddca608a61cad0482f1564
nehalRayuu <99276671+nehalRayuu@users.noreply.github.com>
2023-08-27 18:18:36 +0530

    Update script.js

--- task/script.js
+++ task/script.js
@@ -2,7 +2,13 @@ const questions = document.querySelectorAll('.question');
 
     questions.forEach(question => {
       question.addEventListener('click', () => {
-        const answer = question.nextElementSibling;
-        answer.classList.toggle('answer');
+        // const answer = question.nextElementSibling;
+        // const arrow =question.parentNode.parentNode.lastElementChild;
+       const ans = question.nextElementSibling
+       const icon =question.childNodes[3]
+       console.log(icon)
+      
+        ans.classList.toggle('answer');
+        icon.classList.toggle('rotate')
       });
     });
